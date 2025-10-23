eca3ef9bfce37859f591a2d5d5f55fca5f86f0c6
Thuc Chi Do <thucchi.code@gmail.com>
2025-10-23 12:36:58 -0500

    todos, save categories pt 2

--- app.py
+++ app.py
@@ -31,15 +31,34 @@ db: Client = create_client(db_url, db_key)
 def index():
     return render_template("index.html")
 
+@app.route("/clear_session")
+def clear():
+    session.clear()
+    return redirect("/")
+
+# Display pet info
+@app.route("/pet", methods=["GET"])
+def display():
+    if not session.get("fields"):
+        print("HEYY")
+        categories = db.table("info_categories").select("*").order("id").execute().data
+        save_categories(categories)
+    pet_id = request.args.get("id")
+    data = db.table("information").select("*").eq("pet_id", int(pet_id)).order("cat_id").execute().data
+    return render_template("pet.html", id=pet_id, data=data)
+
 @app.route("/create-pet", methods=["POST", "GET"])
-def create_pet1():
-    # Query all categories of info
-    fields = db.table("info_categories").select("*").order("id").execute().data
+def create_pet():
+    # Save categories if not yet
+    if not session.get("fields"):
+        print("HEYY")
+        categories = db.table("info_categories").select("*").order("id").execute().data
+        save_categories(categories)
 
     # If POST
     if request.method == "POST":
         # Hash user password
-        pwd = request.form.get(str(find_cat_id("Password", fields)))
+        pwd = request.form.get(str(find_cat_id("Password")))
         hashed_pwd = generate_password_hash(pwd)
 
         # Insert pet into data, get pet id
@@ -49,14 +68,22 @@ def create_pet1():
         # Join all info into list of dicts
         data = []
         for cat,val in request.form.items():
-            if (cat != str(find_cat_id("Password", fields))) and (val != ""):
+            if (cat != str(find_cat_id("Password"))) and (val != ""):
                 data.append({"pet_id": pet_id.get("id"), "cat_id": cat, "info": val})
         
         # Add info to database
         db.table("information").insert(data).execute()
 
-        # Return to homepage
+        # Save pet id to cookies to create qr
+        session["pet_id"] = pet_id.get("id")
+
+        # TODO 2: Call func that creates qr code
+        create_qr()
+
+        # TODO 5: Redirect to page that displays qr code with option to download as png (redirect to route in todo 4)
         return redirect("/")
     
     # Go to page
-    return render_template("create_pet.html", fields=fields)
+    return render_template("create_pet.html", fields=session["fields"])
+
+# TODO 4: Create a route (return render_template()) to the page that displays qr code (route to qr_display.html in todo 3)
--- helpers.py
+++ helpers.py
@@ -7,14 +7,25 @@ def secret_key():
     import secrets
     print(secrets.token_hex(32))
 
-def find_cat_id(cat, fields):
-    for f in fields:
-        if f["category"] == cat:
-            return f["id"]
+def find_cat_id(cat):
+    for id,value in session.get("fields", {}).items():
+        if value["name"] == cat:
+            return id
     return -1
 
-def find_id_cat(id, fields):
-    for f in fields:
-        if f["id"] == id:
-            return f["category"]
-    return -1
+def match_categories(data, categories):
+    # for d in data:
+    #     cat = find_id_cat(d["cat_id"], categories)
+    pass
+
+def save_categories(categories):
+    fields = {}
+    for c in categories:
+        fields[c["id"]] = {"name": c["category"], "req": c["req"], "descr": c["descr"]}
+    session["fields"] = fields
+
+# TODO 1: Create the qr code to url
+def create_qr():
+    # Url to .../pet?id=...
+    # id is saved in session["pet_id"]
+    pass
