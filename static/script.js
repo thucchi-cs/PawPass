0d86711d80163a502801a281bf3f09aca025bac9
Thuc Chi Do <thucchi.code@gmail.com>
2025-10-26 18:29:04 -0500

    style home and create

---
+++ static/script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#2563eb', // elegant blue
+            accent: '#1e40af'
+          }
+        }
+      }
+    }
