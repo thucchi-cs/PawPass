583cb7dd2b54363e3a622d7852a3abe4d41a47c1
Ronin-the-Barbarian <>
2025-10-26 15:59:04 -0500

    WOW

---
+++ static/script.js
@@ -0,0 +1,50 @@
+function flashMsg(msg) {
+    let flash = document.querySelector("#flash-msg")
+    flash.hidden = false;
+    flash.querySelector("#msg").innerHTML = msg
+}
+
+document.querySelector(".btn-close").addEventListener("click", () => {
+    document.querySelector("#flash-msg").hidden = true;
+})
+
+if ((window.location.pathname == "/create-pet") || (window.location.pathname == "/edit-pet")){
+    // Get form to check validation
+    let addPet = document.querySelector("#addPet")
+    addPet.querySelector("#btn").addEventListener("click", function() {
+        // Get inputs
+        let petName = addPet.querySelector("#id1").value;
+        let petType = addPet.querySelector("#id2").value;
+        let favoriteFood = addPet.querySelector("#id5").value;
+        let OwnerName = addPet.querySelector("#id7").value;
+        let OwnerContact = addPet.querySelector("#id8").value;
+        let petAge = addPet.querySelector("#id4").value;
+        let password = addPet.querySelector("#id11").value;
+
+
+
+
+
+        // Flash error if not all fields are filled out
+        if (!petName || !petType || !favoriteFood || !OwnerName || !OwnerContact || !password) {
+            flashMsg("All required fields must be filled out!");
+            console.log(petName + petType + favoriteFood + OwnerName + OwnerContact + password)
+            return;
+        }
+    
+        if (petAge != null && /^\d+$/.test(petAge) == false){
+            flashMsg("Your pet age is not a valid number")
+            return;
+        }
+    
+        // Flash error for invalid date
+        const passwordRight = password.match(/[^a-zA-Z]/g) || [];
+        console.log(passwordRight)
+        if(password.length < 8 || passwordRight.length == 0){
+            flashMsg("Your password does not meet the requirements. You must have a password 8 characters or longer, and it must have at least 1 number or 1 special character")
+            return;
+        }
+
+        // Submit form if all inputs are valid
+        addPet.submit()
+    })}
